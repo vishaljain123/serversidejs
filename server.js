2774fcd0f08e14420f57a06879e2754d8629c2d9
vishaljain123 <vishal790@outlook.com>
2017-02-21 14:54:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// app.get('/', function(req, res){
+//   res.sendFile('/Users/maansoftwares-02/Desktop/latest/chat/testingpart2/simple.html');
+// });
+var clients;
+
+
+console.log("hey user you are most welcome!!!");
+io.on('connection', function(socket){
+
+	console.log("use is coneected"+ socket.id);
+
+socket.on('join',function(uname){
+
+	clients=uname;
+});	
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg,clients,socket.id);
+  });
+
+  socket.on("disconnect",function(){
+
+  		console.log("use disconnected");
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
