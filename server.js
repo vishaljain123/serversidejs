4e60e75be19993c281238bf79b8fe2b291dab9b3
vishaljain123 <vishal790@outlook.com>
2017-02-24 21:09:46 +0530

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var app = require('express')();
-var http = require('http');
+//var app = require('express')();
+
 //var httpio=http.Server(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(httpio);
 
 
 
@@ -20,7 +20,8 @@ var io = require('socket.io')(http);
 
 
 //var http1 = require('http');
-
+var http = require('http');
+var io=require('socket.io')(http);
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
@@ -30,20 +31,20 @@ console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
 
 
 // console.log("hey user you are most welcome!!!");
-io.on('connection', function(socket){
+// io.on('connection', function(socket){
 
-	console.log("user is coneected"+ socket.id);
+// 	console.log("user is coneected"+ socket.id);
 
-socket.on('join',function(uname){
+// socket.on('join',function(uname){
 
-	clients=uname;
-});	
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg,clients,socket.id);
-  });
+// 	clients=uname;
+// });	
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg,clients,socket.id);
+//   });
 
-  socket.on("disconnect",function(){
+//   socket.on("disconnect",function(){
 
-  		console.log("use disconnected");
-  });
-});
+//   		console.log("use disconnected");
+//   });
+// });
