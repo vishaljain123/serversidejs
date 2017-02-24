e7c9a5387d1dd0d83eb093e3825c2cf77d5ac753
vishaljain123 <vishal790@outlook.com>
2017-02-24 21:19:15 +0530

    commit

--- server.js
+++ server.js
@@ -30,21 +30,21 @@ console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
 
 var io=require('socket.io')(http);
 
-// console.log("hey user you are most welcome!!!");
-// io.on('connection', function(socket){
+console.log("hey user you are most welcome!!!");
+io.on('connection', function(socket){
 
-// 	console.log("user is coneected"+ socket.id);
+	console.log("user is coneected"+ socket.id);
 
-// socket.on('join',function(uname){
+socket.on('join',function(uname){
 
-// 	clients=uname;
-// });	
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg,clients,socket.id);
-//   });
+	clients=uname;
+});	
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg,clients,socket.id);
+  });
 
-//   socket.on("disconnect",function(){
+  socket.on("disconnect",function(){
 
-//   		console.log("use disconnected");
-//   });
-// });
+  		console.log("use disconnected");
+  });
+});
